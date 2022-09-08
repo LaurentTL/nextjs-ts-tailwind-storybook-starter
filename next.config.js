b9484d897774f9c814c2a2487074459962af41ae
Laurent Treuil <laurent.treuil@gmail.com>
2022-09-08 12:32:16 +0200

    build: storybook tailwind

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Next webpackconfig solve BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default.
+  // future: {
+  //   webpack5: true,
+  // },
+  webpack: (config) => {
+    config.resolve.fallback = { util: false };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
